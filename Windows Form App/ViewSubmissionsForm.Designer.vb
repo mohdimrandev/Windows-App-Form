c9b61d83eaccbd71845531a2cab911a975080055
Imran <imranmohd2209@gmail.com>
2024-06-21 19:26:01 +0530

    Added ViewSubmissionsForm files

---
+++ Windows Form App/ViewSubmissionsForm.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ViewSubmissionsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnSave = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.BtnSearch = New System.Windows.Forms.Button()
+        Me.TxtSearchEmail = New System.Windows.Forms.TextBox()
+        Me.BtnDelete = New System.Windows.Forms.Button()
+        Me.TxtStopwatch = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LblGitHub = New System.Windows.Forms.Label()
+        Me.LblPhone = New System.Windows.Forms.Label()
+        Me.LblEmail = New System.Windows.Forms.Label()
+        Me.LblName = New System.Windows.Forms.Label()
+        Me.TxtGithub = New System.Windows.Forms.TextBox()
+        Me.TxtEmail = New System.Windows.Forms.TextBox()
+        Me.TxtPhone = New System.Windows.Forms.TextBox()
+        Me.BtnNext = New System.Windows.Forms.Button()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.BtnPrevious = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnSave
+        '
+        Me.BtnSave.BackColor = System.Drawing.Color.Lime
+        Me.BtnSave.Location = New System.Drawing.Point(147, 330)
+        Me.BtnSave.Name = "BtnSave"
+        Me.BtnSave.Size = New System.Drawing.Size(149, 24)
+        Me.BtnSave.TabIndex = 39
+        Me.BtnSave.Text = "SAVE  (Ctrl + S)"
+        Me.BtnSave.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label2.Location = New System.Drawing.Point(33, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(377, 17)
+        Me.Label2.TabIndex = 38
+        Me.Label2.Text = "Mohammed Imran, Slidely Task 2 - View Submissions Form"
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.BackColor = System.Drawing.Color.Orange
+        Me.BtnEdit.Location = New System.Drawing.Point(41, 300)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(149, 24)
+        Me.BtnEdit.TabIndex = 37
+        Me.BtnEdit.Text = "EDIT (Ctrl  + E)"
+        Me.BtnEdit.UseVisualStyleBackColor = False
+        '
+        'BtnSearch
+        '
+        Me.BtnSearch.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.BtnSearch.Location = New System.Drawing.Point(280, 49)
+        Me.BtnSearch.Name = "BtnSearch"
+        Me.BtnSearch.Size = New System.Drawing.Size(142, 24)
+        Me.BtnSearch.TabIndex = 36
+        Me.BtnSearch.Text = "SEARCH  (Ctrl + F)"
+        Me.BtnSearch.UseVisualStyleBackColor = False
+        '
+        'TxtSearchEmail
+        '
+        Me.TxtSearchEmail.Location = New System.Drawing.Point(52, 52)
+        Me.TxtSearchEmail.Name = "TxtSearchEmail"
+        Me.TxtSearchEmail.Size = New System.Drawing.Size(179, 20)
+        Me.TxtSearchEmail.TabIndex = 35
+        '
+        'BtnDelete
+        '
+        Me.BtnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.BtnDelete.Location = New System.Drawing.Point(246, 300)
+        Me.BtnDelete.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnDelete.Name = "BtnDelete"
+        Me.BtnDelete.Size = New System.Drawing.Size(149, 24)
+        Me.BtnDelete.TabIndex = 34
+        Me.BtnDelete.Text = "DELETE (Ctrl + D)"
+        Me.BtnDelete.UseVisualStyleBackColor = False
+        '
+        'TxtStopwatch
+        '
+        Me.TxtStopwatch.Location = New System.Drawing.Point(201, 227)
+        Me.TxtStopwatch.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtStopwatch.Name = "TxtStopwatch"
+        Me.TxtStopwatch.ReadOnly = True
+        Me.TxtStopwatch.Size = New System.Drawing.Size(200, 20)
+        Me.TxtStopwatch.TabIndex = 33
+        Me.TxtStopwatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(39, 228)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 17)
+        Me.Label1.TabIndex = 32
+        Me.Label1.Text = "Stopwatch Time"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblGitHub
+        '
+        Me.LblGitHub.AutoSize = True
+        Me.LblGitHub.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.LblGitHub.Location = New System.Drawing.Point(39, 189)
+        Me.LblGitHub.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LblGitHub.Name = "LblGitHub"
+        Me.LblGitHub.Size = New System.Drawing.Size(152, 17)
+        Me.LblGitHub.TabIndex = 31
+        Me.LblGitHub.Text = "Github Link For Task 2"
+        Me.LblGitHub.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblPhone
+        '
+        Me.LblPhone.AutoSize = True
+        Me.LblPhone.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.LblPhone.Location = New System.Drawing.Point(39, 149)
+        Me.LblPhone.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LblPhone.Name = "LblPhone"
+        Me.LblPhone.Size = New System.Drawing.Size(82, 17)
+        Me.LblPhone.TabIndex = 30
+        Me.LblPhone.Text = "Phone Num"
+        Me.LblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblEmail
+        '
+        Me.LblEmail.AutoSize = True
+        Me.LblEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.LblEmail.Location = New System.Drawing.Point(39, 114)
+        Me.LblEmail.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LblEmail.Name = "LblEmail"
+        Me.LblEmail.Size = New System.Drawing.Size(42, 17)
+        Me.LblEmail.TabIndex = 29
+        Me.LblEmail.Text = "Email"
+        Me.LblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'LblName
+        '
+        Me.LblName.AutoSize = True
+        Me.LblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.LblName.Location = New System.Drawing.Point(39, 80)
+        Me.LblName.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LblName.Name = "LblName"
+        Me.LblName.Size = New System.Drawing.Size(45, 17)
+        Me.LblName.TabIndex = 28
+        Me.LblName.Text = "Name"
+        Me.LblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TxtGithub
+        '
+        Me.TxtGithub.Location = New System.Drawing.Point(201, 188)
+        Me.TxtGithub.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtGithub.Name = "TxtGithub"
+        Me.TxtGithub.ReadOnly = True
+        Me.TxtGithub.Size = New System.Drawing.Size(200, 20)
+        Me.TxtGithub.TabIndex = 27
+        Me.TxtGithub.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TxtEmail
+        '
+        Me.TxtEmail.Location = New System.Drawing.Point(201, 113)
+        Me.TxtEmail.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtEmail.Name = "TxtEmail"
+        Me.TxtEmail.ReadOnly = True
+        Me.TxtEmail.Size = New System.Drawing.Size(200, 20)
+        Me.TxtEmail.TabIndex = 26
+        Me.TxtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TxtPhone
+        '
+        Me.TxtPhone.Location = New System.Drawing.Point(201, 148)
+        Me.TxtPhone.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtPhone.Name = "TxtPhone"
+        Me.TxtPhone.ReadOnly = True
+        Me.TxtPhone.Size = New System.Drawing.Size(200, 20)
+        Me.TxtPhone.TabIndex = 25
+        Me.TxtPhone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'BtnNext
+        '
+        Me.BtnNext.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.BtnNext.Location = New System.Drawing.Point(246, 262)
+        Me.BtnNext.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnNext.Name = "BtnNext"
+        Me.BtnNext.Size = New System.Drawing.Size(149, 24)
+        Me.BtnNext.TabIndex = 24
+        Me.BtnNext.Text = "NEXT (Ctrl + N)"
+        Me.BtnNext.UseVisualStyleBackColor = False
+        '
+        'TxtName
+        '
+        Me.TxtName.Location = New System.Drawing.Point(201, 80)
+        Me.TxtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.ReadOnly = True
+        Me.TxtName.Size = New System.Drawing.Size(200, 20)
+        Me.TxtName.TabIndex = 23
+        Me.TxtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'BtnPrevious
+        '
+        Me.BtnPrevious.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.BtnPrevious.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.BtnPrevious.Location = New System.Drawing.Point(41, 262)
+        Me.BtnPrevious.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnPrevious.Name = "BtnPrevious"
+        Me.BtnPrevious.Size = New System.Drawing.Size(149, 24)
+        Me.BtnPrevious.TabIndex = 22
+        Me.BtnPrevious.Text = "PREVIOUS (Ctrl + P)"
+        Me.BtnPrevious.UseVisualStyleBackColor = False
+        '
+        'ViewSubmissionsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 366)
+        Me.Controls.Add(Me.BtnSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnSearch)
+        Me.Controls.Add(Me.TxtSearchEmail)
+        Me.Controls.Add(Me.BtnDelete)
+        Me.Controls.Add(Me.TxtStopwatch)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LblGitHub)
+        Me.Controls.Add(Me.LblPhone)
+        Me.Controls.Add(Me.LblEmail)
+        Me.Controls.Add(Me.LblName)
+        Me.Controls.Add(Me.TxtGithub)
+        Me.Controls.Add(Me.TxtEmail)
+        Me.Controls.Add(Me.TxtPhone)
+        Me.Controls.Add(Me.BtnNext)
+        Me.Controls.Add(Me.TxtName)
+        Me.Controls.Add(Me.BtnPrevious)
+        Me.Name = "ViewSubmissionsForm"
+        Me.Text = "ViewSubmissionsForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnSave As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents BtnSearch As Button
+    Friend WithEvents TxtSearchEmail As TextBox
+    Friend WithEvents BtnDelete As Button
+    Friend WithEvents TxtStopwatch As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LblGitHub As Label
+    Friend WithEvents LblPhone As Label
+    Friend WithEvents LblEmail As Label
+    Friend WithEvents LblName As Label
+    Friend WithEvents TxtGithub As TextBox
+    Friend WithEvents TxtEmail As TextBox
+    Friend WithEvents TxtPhone As TextBox
+    Friend WithEvents BtnNext As Button
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents BtnPrevious As Button
+End Class
---
+++ Windows Form App/ViewSubmissionsForm.vb
@@ -0,0 +1,289 @@
+﻿Imports System.Net.Http
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports Newtonsoft.Json
+
+Public Class ViewSubmissionsForm
+    Public Class Submission
+        Public Property Name As String
+        Public Property Email As String
+        Public Property Phone As String
+
+        <JsonProperty("github_link")>
+        Public Property GithubLink As String
+
+        <JsonProperty("stopwatch_time")>
+        Public Property StopwatchTime As String
+
+        Public Sub New(name As String, email As String, phone As String, githubLink As String, stopwatchTime As String)
+            Me.Name = name
+            Me.Email = email
+            Me.Phone = phone
+            Me.GithubLink = githubLink
+            Me.StopwatchTime = stopwatchTime
+        End Sub
+    End Class
+
+    ' List to hold submissions
+    Private submissions As List(Of Submission)
+    Private currentIndex As Integer
+
+    Private Const placeholderText As String = "Search by email"
+
+    Private Sub ViewSubmissionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "View Submissions"
+        Me.KeyPreview = True
+
+        TxtSearchEmail.Text = placeholderText
+        TxtSearchEmail.ForeColor = Color.Gray
+
+        BtnSave.Enabled = False
+
+        ' Fetch submissions from the backend
+        FetchSubmissionsAsync()
+    End Sub
+
+    ' Constructor
+    Public Sub New()
+
+        InitializeComponent()
+
+        ' Initialize submissions list
+        Me.submissions = New List(Of Submission)()
+        Me.Text = "View Submissions"
+        currentIndex = 0
+    End Sub
+
+    ' Method to fetch submissions from the backend
+    Private Async Sub FetchSubmissionsAsync()
+        Dim apiUrl As String = "http://localhost:3000/read"
+        Try
+            Using client As New HttpClient()
+                Dim response As HttpResponseMessage = Await client.GetAsync(apiUrl)
+                If response.IsSuccessStatusCode Then
+                    Dim jsonResponse As String = Await response.Content.ReadAsStringAsync()
+                    Me.submissions = JsonConvert.DeserializeObject(Of List(Of Submission))(jsonResponse)
+
+                    ' Display the first submission (if available)
+                    If submissions.Count > 0 Then
+                        DisplaySubmission(currentIndex)
+                    End If
+                Else
+                    MessageBox.Show("Failed to fetch submissions.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error fetching data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Method to display submission at specified index
+    Private Sub DisplaySubmission(index As Integer)
+        If index < 0 OrElse index >= submissions.Count Then
+            Return
+        End If
+
+        Dim submission = submissions(index)
+        TxtName.Text = submission.Name
+        TxtEmail.Text = submission.Email
+        TxtPhone.Text = submission.Phone
+        TxtGithub.Text = submission.GithubLink
+        TxtStopwatch.Text = submission.StopwatchTime
+
+        ' Disable Save button after displaying a submission
+        BtnSave.Enabled = False
+    End Sub
+
+    ' Event handler for Edit button click
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        ' Enable editing of text fields
+        TxtName.ReadOnly = False
+        TxtEmail.ReadOnly = False
+        TxtPhone.ReadOnly = False
+        TxtGithub.ReadOnly = False
+        TxtStopwatch.ReadOnly = False
+
+        BtnSave.Enabled = True
+        BtnSave.Text = "Save"
+    End Sub
+
+    ' Event handler for Save button click (after editing)
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+
+        ' Get updated data from text fields
+        Dim updatedName As String = TxtName.Text
+        Dim updatedEmail As String = TxtEmail.Text
+        Dim updatedPhone As String = TxtPhone.Text
+        Dim updatedGithub As String = TxtGithub.Text
+        Dim updatedStopwatch As String = TxtStopwatch.Text
+
+        If Not IsValidEmail(updatedEmail) Then
+            MessageBox.Show("Please enter a valid email address.", "Invalid Email", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Validate phone number
+        If Not IsValidPhone(updatedPhone) Then
+            MessageBox.Show("Please enter a valid phone number. Only numeric values are allowed.", "Invalid Phone", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Make PUT request to update backend
+        UpdateSubmission(updatedName, updatedEmail, updatedPhone, updatedGithub, updatedStopwatch)
+
+        ' Optionally, disable editing mode and reset UI
+        TxtName.ReadOnly = True
+        TxtEmail.ReadOnly = True
+        TxtPhone.ReadOnly = True
+        TxtGithub.ReadOnly = True
+        TxtStopwatch.ReadOnly = True
+
+        ' Disable Save button after saving
+        BtnSave.Enabled = False
+    End Sub
+
+    ' Method to update submission via API
+    Private Async Sub UpdateSubmission(name As String, email As String, phone As String, githubLink As String, stopwatchTime As String)
+        Dim apiUrl As String = $"http://localhost:3000/edit/{Uri.EscapeDataString(email)}"
+
+        Try
+            Using client As New HttpClient()
+                Dim postData As String = $"{{""name"": ""{name}"", ""email"": ""{email}"", ""phone"": ""{phone}"", ""github_link"": ""{githubLink}"", ""stopwatch_time"": ""{stopwatchTime}""}}"
+                Dim content As New StringContent(postData, System.Text.Encoding.UTF8, "application/json")
+
+                Dim response = Await client.PutAsync(apiUrl, content)
+
+                If response.IsSuccessStatusCode Then
+                    MessageBox.Show("Submission updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Failed to update submission.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error updating submission: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function IsValidEmail(email As String) As Boolean
+        Dim pattern As String = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        Dim regex As New Regex(pattern)
+        Return regex.IsMatch(email)
+    End Function
+
+    Private Function IsValidPhone(phone As String) As Boolean
+        Dim pattern As String = "^\d+$"
+        Dim regex As New Regex(pattern)
+        Return regex.IsMatch(phone)
+    End Function
+
+    ' Event handler for Previous button click
+    Private Sub BtnPrevious_Click(sender As Object, e As EventArgs) Handles BtnPrevious.Click
+        If currentIndex > 0 Then
+            currentIndex -= 1
+            DisplaySubmission(currentIndex)
+        End If
+    End Sub
+
+    ' Event handler for Next button click
+    Private Sub BtnNext_Click(sender As Object, e As EventArgs) Handles BtnNext.Click
+        If currentIndex < submissions.Count - 1 Then
+            currentIndex += 1
+            DisplaySubmission(currentIndex)
+        End If
+    End Sub
+
+    ' Keyboard shortcut handling for Previous, Next, Edit, Save
+    Private Sub ViewSubmissionsForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.P Then
+            BtnPrevious_Click(sender, e)
+        ElseIf e.Control AndAlso e.KeyCode = Keys.N Then
+            BtnNext_Click(sender, e)
+        ElseIf e.Control AndAlso e.KeyCode = Keys.F Then
+            BtnSearch_Click(sender, e)
+        ElseIf e.Control AndAlso e.KeyCode = Keys.D Then
+            BtnDelete_Click(sender, e)
+        ElseIf e.Control AndAlso e.KeyCode = Keys.E Then
+            BtnEdit_Click(sender, e)
+        ElseIf e.Control AndAlso e.KeyCode = Keys.S Then
+            BtnSave_Click(sender, e)
+        End If
+    End Sub
+
+    ' Event handler for Search button click
+    Private Async Sub BtnSearch_Click(sender As Object, e As EventArgs) Handles BtnSearch.Click
+        Dim email As String = TxtSearchEmail.Text.Trim()
+        If email = placeholderText OrElse String.IsNullOrEmpty(email) Then
+            MessageBox.Show("Please enter an email to search.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim apiUrl As String = $"http://localhost:3000/search?email={Uri.EscapeDataString(email)}"
+        Try
+            Using client As New HttpClient()
+                Dim response As HttpResponseMessage = Await client.GetAsync(apiUrl)
+                If response.IsSuccessStatusCode Then
+                    Dim jsonResponse As String = Await response.Content.ReadAsStringAsync()
+                    Dim searchResults = JsonConvert.DeserializeObject(Of List(Of Submission))(jsonResponse)
+                    If searchResults.Count > 0 Then
+                        submissions = searchResults
+                        currentIndex = 0
+                        DisplaySubmission(currentIndex)
+                    Else
+                        MessageBox.Show("No submissions found for the given email.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                Else
+                    MessageBox.Show("Failed to search submissions.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error searching submissions: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Event handler for Delete button click
+    Private Async Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Dim apiUrl As String = $"http://localhost:3000/delete?email={Uri.EscapeDataString(submissions(currentIndex).Email)}"
+        Try
+            Using client As New HttpClient()
+                Dim response As HttpResponseMessage = Await client.DeleteAsync(apiUrl)
+                If response.IsSuccessStatusCode Then
+                    submissions.RemoveAt(currentIndex)
+                    If currentIndex > 0 Then currentIndex -= 1
+                    If submissions.Count > 0 Then
+                        DisplaySubmission(currentIndex)
+                    Else
+                        ClearSubmissionFields()
+                    End If
+                    MessageBox.Show("Submission deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Failed to delete submission.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error deleting submission: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Methods to handle placeholder text
+    Private Sub TxtSearchEmail_GotFocus(sender As Object, e As EventArgs) Handles TxtSearchEmail.GotFocus
+        If TxtSearchEmail.Text = placeholderText Then
+            TxtSearchEmail.Text = ""
+            TxtSearchEmail.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TxtSearchEmail_LostFocus(sender As Object, e As EventArgs) Handles TxtSearchEmail.LostFocus
+        If String.IsNullOrWhiteSpace(TxtSearchEmail.Text) Then
+            TxtSearchEmail.Text = placeholderText
+            TxtSearchEmail.ForeColor = Color.Gray
+        End If
+    End Sub
+
+    Private Sub ClearSubmissionFields()
+        TxtName.Text = ""
+        TxtEmail.Text = ""
+        TxtPhone.Text = ""
+        TxtGithub.Text = ""
+        TxtStopwatch.Text = ""
+    End Sub
+End Class
