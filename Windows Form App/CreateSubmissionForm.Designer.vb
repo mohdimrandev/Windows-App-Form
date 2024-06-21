4e4e6522816c9e05206c10084e83122086db2f2a
Imran <imranmohd2209@gmail.com>
2024-06-21 19:27:39 +0530

    Added CreateSubmissionForm files

---
+++ Windows Form App/CreateSubmissionForm.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CreateSubmissionForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TxtStopwatch = New System.Windows.Forms.TextBox()
+        Me.TxtGithub = New System.Windows.Forms.TextBox()
+        Me.TxtPhone = New System.Windows.Forms.TextBox()
+        Me.TxtEmail = New System.Windows.Forms.TextBox()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnSubmit = New System.Windows.Forms.Button()
+        Me.BtnToggleStopwatch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label5.Location = New System.Drawing.Point(28, 37)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(383, 17)
+        Me.Label5.TabIndex = 33
+        Me.Label5.Text = "Mohammed Imran, Slidely Task 2 - Create Submission Form"
+        '
+        'TxtStopwatch
+        '
+        Me.TxtStopwatch.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.TxtStopwatch.Location = New System.Drawing.Point(263, 262)
+        Me.TxtStopwatch.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtStopwatch.Name = "TxtStopwatch"
+        Me.TxtStopwatch.ReadOnly = True
+        Me.TxtStopwatch.Size = New System.Drawing.Size(105, 23)
+        Me.TxtStopwatch.TabIndex = 32
+        Me.TxtStopwatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TxtGithub
+        '
+        Me.TxtGithub.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.TxtGithub.Location = New System.Drawing.Point(203, 217)
+        Me.TxtGithub.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtGithub.Name = "TxtGithub"
+        Me.TxtGithub.Size = New System.Drawing.Size(200, 23)
+        Me.TxtGithub.TabIndex = 31
+        '
+        'TxtPhone
+        '
+        Me.TxtPhone.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.TxtPhone.Location = New System.Drawing.Point(203, 184)
+        Me.TxtPhone.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtPhone.Name = "TxtPhone"
+        Me.TxtPhone.Size = New System.Drawing.Size(200, 23)
+        Me.TxtPhone.TabIndex = 30
+        '
+        'TxtEmail
+        '
+        Me.TxtEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.TxtEmail.Location = New System.Drawing.Point(203, 148)
+        Me.TxtEmail.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtEmail.Name = "TxtEmail"
+        Me.TxtEmail.Size = New System.Drawing.Size(200, 23)
+        Me.TxtEmail.TabIndex = 29
+        '
+        'TxtName
+        '
+        Me.TxtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.TxtName.Location = New System.Drawing.Point(203, 105)
+        Me.TxtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.Size = New System.Drawing.Size(200, 23)
+        Me.TxtName.TabIndex = 28
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label4.Location = New System.Drawing.Point(41, 220)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(152, 17)
+        Me.Label4.TabIndex = 27
+        Me.Label4.Text = "Github Link For Task 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label3.Location = New System.Drawing.Point(41, 184)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 17)
+        Me.Label3.TabIndex = 26
+        Me.Label3.Text = "Phone Num"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label2.Location = New System.Drawing.Point(41, 148)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 17)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Email"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(41, 105)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 17)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "Name"
+        '
+        'BtnSubmit
+        '
+        Me.BtnSubmit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BtnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.BtnSubmit.Location = New System.Drawing.Point(136, 305)
+        Me.BtnSubmit.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnSubmit.Name = "BtnSubmit"
+        Me.BtnSubmit.Size = New System.Drawing.Size(149, 24)
+        Me.BtnSubmit.TabIndex = 23
+        Me.BtnSubmit.Text = "SUBMIT (Ctrl + S)"
+        Me.BtnSubmit.UseVisualStyleBackColor = False
+        '
+        'BtnToggleStopwatch
+        '
+        Me.BtnToggleStopwatch.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.BtnToggleStopwatch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.BtnToggleStopwatch.Location = New System.Drawing.Point(46, 262)
+        Me.BtnToggleStopwatch.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnToggleStopwatch.Name = "BtnToggleStopwatch"
+        Me.BtnToggleStopwatch.Size = New System.Drawing.Size(199, 24)
+        Me.BtnToggleStopwatch.TabIndex = 22
+        Me.BtnToggleStopwatch.Text = "TOGGLE STOPWATCH (Ctrl  + T)"
+        Me.BtnToggleStopwatch.UseVisualStyleBackColor = False
+        '
+        'CreateSubmissionForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 366)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TxtStopwatch)
+        Me.Controls.Add(Me.TxtGithub)
+        Me.Controls.Add(Me.TxtPhone)
+        Me.Controls.Add(Me.TxtEmail)
+        Me.Controls.Add(Me.TxtName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnSubmit)
+        Me.Controls.Add(Me.BtnToggleStopwatch)
+        Me.Name = "CreateSubmissionForm"
+        Me.Text = "CreateSubmissionForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TxtStopwatch As TextBox
+    Friend WithEvents TxtGithub As TextBox
+    Friend WithEvents TxtPhone As TextBox
+    Friend WithEvents TxtEmail As TextBox
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnSubmit As Button
+    Friend WithEvents BtnToggleStopwatch As Button
+End Class
---
+++ Windows Form App/CreateSubmissionForm.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Net.Http
+
+Public Class CreateSubmissionForm
+
+    Public Class Submission
+        Public Property Name As String
+        Public Property Email As String
+        Public Property Phone As String
+        Public Property GithubLink As String
+        Public Property StopwatchTime As String
+
+        Public Sub New(name As String, email As String, phone As String, githubLink As String, stopwatchTime As String)
+            Me.Name = name
+            Me.Email = email
+            Me.Phone = phone
+            Me.GithubLink = githubLink
+            Me.StopwatchTime = stopwatchTime
+        End Sub
+    End Class
+
+    Private stopwatch As New System.Diagnostics.Stopwatch()
+    Private WithEvents TimerUpdate As New Timer()
+
+    Private Sub CreateSubmissionForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Create New Submission"
+        Me.KeyPreview = True
+        TxtStopwatch.Text = "00:00:00"
+    End Sub
+
+    ' Submit button click event
+    Private Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+
+        Dim name As String = TxtName.Text.Trim()
+        Dim email As String = TxtEmail.Text.Trim()
+        Dim phone As String = TxtPhone.Text.Trim()
+        Dim githubLink As String = TxtGithub.Text.Trim()
+
+        If Not IsValidEmail(email) Then
+            MessageBox.Show("Please enter a valid email address.", "Invalid Email", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        If Not IsValidPhone(phone) Then
+            MessageBox.Show("Please enter a valid phone number. Only numeric values are allowed.", "Invalid Phone", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Call API to submit data
+        CallSubmitAPI(name, email, phone, githubLink)
+    End Sub
+
+    ' Validate email format
+    Private Function IsValidEmail(email As String) As Boolean
+        Dim pattern As String = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        Dim regex As New Regex(pattern)
+        Return regex.IsMatch(email)
+    End Function
+
+    ' Validate phone number format (numeric only)
+    Private Function IsValidPhone(phone As String) As Boolean
+        Dim pattern As String = "^\d+$"
+        Dim regex As New Regex(pattern)
+        Return regex.IsMatch(phone)
+    End Function
+
+    ' Call API to submit data
+    Private Sub CallSubmitAPI(name As String, email As String, phone As String, githubLink As String)
+        Dim apiUrl As String = "http://localhost:3000/submit"
+
+        Try
+            If stopwatch.IsRunning Then
+                stopwatch.Stop()
+            End If
+
+            Dim stopwatchTime As String = stopwatch.Elapsed.ToString("hh\:mm\:ss")
+
+            ' Prepare HTTP request
+            Using client As New HttpClient()
+                Dim postData As String = $"{{""name"": ""{name}"", ""email"": ""{email}"", ""phone"": ""{phone}"", ""github_link"": ""{githubLink}"", ""stopwatch_time"": ""{stopwatchTime}""}}"
+                Dim content As New StringContent(postData, System.Text.Encoding.UTF8, "application/json")
+
+                ' Send POST request to API
+                Dim response = client.PostAsync(apiUrl, content).Result
+
+                ' Show appropriate message based on response
+                If response.IsSuccessStatusCode Then
+                    MessageBox.Show("Submission successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Submission failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show($"Error submitting data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Handle keyboard shortcuts
+    Private Sub CreateSubmissionForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        ' Check for Ctrl + S shortcut to submit
+        If e.Control AndAlso e.KeyCode = Keys.S Then
+            BtnSubmit_Click(sender, e)
+        End If
+
+        ' Check for Ctrl + T shortcut to toggle stopwatch
+        If e.Control AndAlso e.KeyCode = Keys.T Then
+            BtnToggleStopwatch_Click(sender, e)
+        End If
+    End Sub
+
+    ' Handle stopwatch toggle button click
+    Private Sub BtnToggleStopwatch_Click(sender As Object, e As EventArgs) Handles BtnToggleStopwatch.Click
+        If Not stopwatch.IsRunning Then
+            stopwatch.Start()
+            TimerUpdate.Start()
+        Else
+            stopwatch.Stop()
+            TimerUpdate.Stop()
+        End If
+    End Sub
+
+    ' Update stopwatch display periodically
+    Private Sub TimerUpdate_Tick(sender As Object, e As EventArgs) Handles TimerUpdate.Tick
+        Dim elapsed = stopwatch.Elapsed
+        TxtStopwatch.Text = $"{elapsed.Hours:00}:{elapsed.Minutes:00}:{elapsed.Seconds:00}"
+    End Sub
+
+    Private Sub CreateSubmissionForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If stopwatch.IsRunning Then
+            stopwatch.Stop()
+            TimerUpdate.Stop()
+        End If
+    End Sub
+End Class
